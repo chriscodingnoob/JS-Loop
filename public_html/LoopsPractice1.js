27fe822ef411c860e71bf40e9df71dc3cb3aacde
Arthur <Arthur@LAPTOP-Q3CAFIFH.niacc.edu>
2020-10-15 09:38:55 -0500

    added code to display numbers 1 to 10

--- public_html/LoopsPractice1.js
+++ public_html/LoopsPractice1.js
@@ -4,3 +4,9 @@
 
 document.write("The numbers from 1 to 10 are: <br>");
 
+let counter = 1;
+while (counter <= 10) {
+    document.write(counter + "<br>");
+    counter = counter + 1;
+}
+document.write("Goodbye!");
